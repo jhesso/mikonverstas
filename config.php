b8b1d722aad5f51d02282db996c3282fc8c0b289
Jhesso <juho.hesso@hotmail.com>
2022-11-19 19:36:19 +0200

    initial commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'mikonverstas';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+try
+{
+	$dbConnection = new PDO("mysql:host={$databaseHost};dbname={$databaseName}", $databaseUsername, $databasePassword);
+	$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e)
+{
+	echo $e->getMessage();
+}
+?>
